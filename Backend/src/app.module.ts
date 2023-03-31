f38aad5c45055fce0da2b72ed5862aa5bd6335af
JEstebanPerez <j.esteban.2019@alumnos.urjc.es>
2023-03-31 04:58:28 +0200

    Added MongoDB and it works

---
+++ Backend/src/app.module.ts
@@ -0,0 +1,16 @@
+
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { Module } from '@nestjs/common';
+
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017', { useFindAndModify: false })
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule { 
+
+}
